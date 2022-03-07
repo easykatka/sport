f510c87b0c5f77af16cd9e8dfb8251dd30a204e5
Easykatka <secondjump@bk.ru>
2022-03-07 15:46:38 +0300

    123

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { NODE_ENV } from 'src/shared/constants/env';
+import { NODE_ENV, PORT } from 'src/shared/constants/env';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule.initialize());
-    const PORT = process.env.PORT || 3000;
 
+    //* HMR
     if (module.hot) {
         module.hot.accept();
         module.hot.dispose(() => app.close());
     }
 
+    //* SWAGGER
     const config = new DocumentBuilder()
         .setTitle('Приложение для организации спортивных мероприятий')
         .setDescription('Документация REST API')
@@ -21,7 +22,8 @@ async function bootstrap() {
         .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api/docs', app, document);
-
+    
+    //* SERVER
     await app.listen(PORT, () => console.log('\x1b[32m', `Server is started on port:${PORT} in ${NODE_ENV} mode`));
 }
 bootstrap();
