9e35cea5055c31bb3d7f540534c7e378bdf193c4
Easykatka <secondjump@bk.ru>
2022-03-12 23:11:05 +0300

    123

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 import { fetch } from 'src/shared/utils/fetch';
 import { buildServerSideProps } from 'src/client/ssr/buildServerSideProps';
-import { useFeature } from 'src/client/hooks/useFeatures';
+// import { useFeature } from 'src/client/hooks/useFeatures';
 import { MainLayout } from '../client/layouts/MainLayout';
 
 const Home: FC = () => {
-    const linkFeature = useFeature('blog_link');
+    // const linkFeature = useFeature('blog_link');
     return (
         <MainLayout>
             <div>123</div>
