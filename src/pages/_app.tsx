7a1bfbb780cd994ff70ae1776f52b331aa30e644
Easykatka <secondjump@bk.ru>
2022-03-24 02:56:50 +0300

    333333

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,9 +26,7 @@ class App extends NextApp<AppProps> {
                     <link rel='icon' href='/static/favicon.ico' />
                     <link rel='preconnect' href='https://fonts.googleapis.com' />
                     <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
-                    <link
-                        href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap'
-                        rel='stylesheet'></link>
+                
                 </Head>
 
                 <ThemeProvider theme={theme}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
 
-  render() {
-    return (
-      <Html lang="ru">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html lang='ru'>
+                <Head />
+                <link
+                    href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap'
+                    rel='stylesheet'></link>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
