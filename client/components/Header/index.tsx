b9ff211b23f98db80d3fd648677df2bf1ab516eb
Easykatka <secondjump@bk.ru>
2022-04-08 23:56:07 +0300

    333

--- client/components/Header/index.tsx
+++ client/components/Header/index.tsx
@@ -13,67 +13,69 @@ import styles from './Header.module.scss';
 import { AuthDialog } from '../AuthDialog';
 
 import { Paper, Button, IconButton, Avatar, ListItem, List } from '@mui/material';
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { IStore } from 'client/api/store';
 
 interface IHeader {
     store?: IStore;
 }
 
-export const Header: React.FC<IHeader> = inject('store')(({ store }) => {
-    const { user, toggleShowSidebar } = store;
-    const [authVisible, setAuthVisible] = React.useState(false);
+export const Header: React.FC<IHeader> = inject('store')(
+    observer(({ store }) => {
+        const { user, toggleShowSidebar } = store;
+        const [authVisible, setAuthVisible] = React.useState(false);
 
-    const openAuthDialog = () => {
-        setAuthVisible(true);
-    };
+        const openAuthDialog = () => {
+            setAuthVisible(true);
+        };
 
-    const closeAuthDialog = () => {
-        setAuthVisible(false);
-    };
-
-    useEffect(() => {
-        if (authVisible && user) {
+        const closeAuthDialog = () => {
             setAuthVisible(false);
-        }
-    }, [authVisible, user]);
+        };
 
-    return (
-        <Paper classes={{ root: styles.root }} elevation={0}>
-            <div className='d-flex align-center'>
-                <IconButton onClick={() => toggleShowSidebar()}>
-                    <MenuIcon />
-                </IconButton>
-                <Link href='/'>
-                    <a>
-                        <img height={35} className='mr-20' src='/static/img/logo.png' alt='Logo' />
-                    </a>
-                </Link>
-            </div>
-            <div className='d-flex align-center'>
-                <IconButton>
-                    <MessageIcon />
-                </IconButton>
-                <IconButton>
-                    <NotificationIcon />
-                </IconButton>
-                {user ? (
-                    <>
-                        <Avatar
-                            className={styles.avatar}
-                            alt='Remy Sharp'
-                            src='https://leonardo.osnova.io/5ffeac9a-a0e5-5be6-98af-659bfaabd2a6/-/scale_crop/108x108/-/format/webp/'
-                        />
-                        <ArrowBottom />
-                    </>
-                ) : (
-                    <div className={styles.loginButton} onClick={openAuthDialog}>
-                        <UserIcon />
-                        Войти
-                    </div>
-                )}
-            </div>
-            <AuthDialog onClose={closeAuthDialog} visible={authVisible} />
-        </Paper>
-    );
-});
+        useEffect(() => {
+            if (authVisible && user) {
+                setAuthVisible(false);
+            }
+        }, [authVisible, user]);
+
+        return (
+            <Paper classes={{ root: styles.root }} elevation={0}>
+                <div className='d-flex align-center'>
+                    <IconButton onClick={() => toggleShowSidebar()}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Link href='/'>
+                        <a>
+                            <img height={35} className='mr-20' src='/static/img/logo.png' alt='Logo' />
+                        </a>
+                    </Link>
+                </div>
+                <div className='d-flex align-center'>
+                    <IconButton>
+                        <MessageIcon />
+                    </IconButton>
+                    <IconButton>
+                        <NotificationIcon />
+                    </IconButton>
+                    {user ? (
+                        <>
+                            <Avatar
+                                className={styles.avatar}
+                                alt='Remy Sharp'
+                                src='https://leonardo.osnova.io/5ffeac9a-a0e5-5be6-98af-659bfaabd2a6/-/scale_crop/108x108/-/format/webp/'
+                            />
+                            <ArrowBottom onClick={() => (store.user = null)} />
+                        </>
+                    ) : (
+                        <div className={styles.loginButton} onClick={openAuthDialog}>
+                            <UserIcon />
+                            Войти
+                        </div>
+                    )}
+                </div>
+                <AuthDialog onClose={closeAuthDialog} visible={authVisible} />
+            </Paper>
+        );
+    })
+);
