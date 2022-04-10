df7acd8f8011fc01c845aaa247cee3668a8366b9
Easykatka <secondjump@bk.ru>
2022-04-10 22:46:55 +0300

    333

--- client/api/store.ts
+++ client/api/store.ts
@@ -1,36 +1,42 @@
-import { makeAutoObservable, runInAction } from 'mobx';
+import { action, computed, makeAutoObservable, makeObservable, observable, runInAction } from 'mobx';
 import { enableStaticRendering } from 'mobx-react';
 import { UserDto } from 'shared/types/UserDto';
 enableStaticRendering(typeof window === 'undefined');
 export interface IStore {
-    user: UserDto | null;
-    showSidebar: boolean;
-    toggleShowSidebar: () => void;
+	user: UserDto | null;
+	showSidebar: boolean;
+	toggleShowSidebar: () => void;
+	hasAdminAccess: boolean
 }
 
 class Store {
-    user = null;
-    showSidebar = true;
-    constructor() {
-        makeAutoObservable(this);
-    }
+	user = null;
+	showSidebar = true;
+	constructor() {
+		makeObservable(this, {
+			user: observable,
+			hasAdminAccess: computed,
+			hydrate: action,
+		});
+	}
+	get hasAdminAccess() {
+		return Boolean(this.user?.roles.find(role => role.name === 'admin'));
+	}
 
-    toggleShowSidebar = () => (this.showSidebar = !this.showSidebar);
+	toggleShowSidebar = () => (this.showSidebar = !this.showSidebar);
 
-    hydrate = async (initialState) => {
-        if (!initialState) return;
-        runInAction(() => {
-            this.user = initialState.user;
-			(typeof window !== 'undefined') && console.log('logged user: ', this.user);
-        });
-    };
+	hydrate = async (initialState) => {
+		if (!initialState) return;
+		this.user = initialState.user;
+		(typeof window !== 'undefined') && console.log('logged user: ', this.user);
+	};
 }
 
 let store;
 export function initializeStore(initialState = null) {
-    const _store = store ?? new Store();
-    _store.hydrate(initialState);
-    if (typeof window === 'undefined') return _store;
-    if (!store) store = _store;
-    return _store;
+	const _store = store ?? new Store();
+	_store.hydrate(initialState);
+	if (typeof window === 'undefined') return _store;
+	if (!store) store = _store;
+	return _store;
 }
--- client/components/Sidebar/index.tsx
+++ client/components/Sidebar/index.tsx
@@ -6,33 +6,40 @@ import { TrendingUpOutlined as TrendingIcon, FormatListBulletedOutlined as ListI
 import styles from './Sidebar.module.scss';
 import { useRouter } from 'next/router';
 import LockIcon from '@mui/icons-material/Lock';
+import { inject, observer } from 'mobx-react';
+import { IStore } from 'client/api/store';
 
-const menu = [
-	{ text: 'Администрирование', icon: <LockIcon />, path: '/admin' },
-    { text: 'Игры', icon: <SportsSoccerIcon />, path: '/games' },
-    { text: 'Правила', icon: <ListIcon />, path: '/rules' },
-    { text: 'Рейтинг Игроков', icon: <TrendingIcon />, path: '/rating' },
-];
 
-export const Sidebar: React.FC = () => {
-    const router = useRouter();
+interface SidebarProps {
+	store?: IStore
+}
 
-    return (
-        <div className={styles.menu}>
-            <ul>
-                {menu.map((obj) => (
-                    <li key={obj.path}>
-                        <Link href={obj.path}>
-                            <a>
+export const Sidebar: React.FC<SidebarProps> = inject('store')(observer(({ store }) => {
+	const router = useRouter();
+
+	const menu = [
+		store.hasAdminAccess && { text: 'Администрирование', icon: <LockIcon />, path: '/admin' },
+		{ text: 'Игры', icon: <SportsSoccerIcon />, path: '/games' },
+		{ text: 'Правила', icon: <ListIcon />, path: '/rules' },
+		{ text: 'Рейтинг Игроков', icon: <TrendingIcon />, path: '/rating' },
+	].filter(Boolean);
+
+	return (
+		<div className={styles.menu}>
+			<ul>
+				{menu.map((obj) => (
+					<li key={obj.path}>
+						<Link href={obj.path}>
+							<a>
 								<Button variant={obj.path !== '/' && router.asPath.includes(obj.path) ? 'contained' : 'text'}>
-                                    {obj.icon}
-                                    {obj.text}
-                                </Button>
-                            </a>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+									{obj.icon}
+									{obj.text}
+								</Button>
+							</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}));
