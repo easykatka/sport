9ecf4bac39e6dcb7641611349a14aedfecd5ba6a
Easykatka <secondjump@bk.ru>
2022-06-25 21:55:32 +0300

    32

--- client/api/index.ts
+++ client/api/index.ts
@@ -3,12 +3,6 @@ export const instance = axios.create({
 	baseURL: 'http://localhost:3000/api',
 });
 
-export class BaseService {
-	downloadUrl(property) {
-		
-	}
-}
-
 export { AuthService } from './AuthService';
 export { RoleMappingService } from './RoleMappingService';
 export { RoleService } from './RoleService';
