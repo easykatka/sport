263bc8100568851d214f9e235aed28011d6ad221
Easykatka <secondjump@bk.ru>
2022-06-26 21:54:57 +0300

    32

--- client/components/inputs/imageInput.tsx
+++ client/components/inputs/imageInput.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import { useState, useEffect } from 'react';
+import NextImage from 'next/image';
 
-export const ImageInput = ({ title = 'Загрузить изображение' }) => {
-	const [selectedImage, setSelectedImage] = useState(null);
-	const [imageUrl, setImageUrl] = useState(null);
+
+export const ImageInput = ({ title = 'Загрузить изображение', onChange }) => {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null);
 
 	useEffect(() => {
 		if (selectedImage) {
@@ -12,27 +14,25 @@ export const ImageInput = ({ title = 'Загрузить изображение'
 		}
 	}, [selectedImage]);
 
-	return (
-		<>
-			<input
-				accept="image/*"
-				type="file"
-				id="select-image"
-				style={{ display: 'none' }}
-				onChange={e => setSelectedImage(e.target.files[0])}
-			/>
-			<label htmlFor="select-image">
-				<Button variant="contained" color="primary" component="span" className='mb-20'>
-					{title}
-				</Button>
-			</label>
-			{imageUrl && selectedImage && (
-				<Box mt={2} textAlign="center">
-					<div>Предпросмотр:</div>
-					<img src={imageUrl} alt={selectedImage.name} height="200px" max-width='200px' />
-				</Box>
-			)}
-		</>
-	);
-};
+    const handleCapture = (e) => {
+        setSelectedImage(e.target.files[0]);
+        onChange?.();
+    };
 
+    return (
+        <>
+            <input accept='image/*' type='file' id='select-image' style={{ display: 'none' }} onChange={handleCapture} />
+            <label htmlFor='select-image'>
+                <Button variant='contained' color='primary' component='span' className='mb-20'>
+                    {title}
+                </Button>
+            </label>
+            {imageUrl && selectedImage && (
+                <Box mt={2} textAlign='center'>
+                    <div>Предпросмотр:</div>
+                    <NextImage src={imageUrl} alt={selectedImage.name} height='300px' width='300px' layout='responsive' />
+                </Box>
+            )}
+        </>
+    );
+};
